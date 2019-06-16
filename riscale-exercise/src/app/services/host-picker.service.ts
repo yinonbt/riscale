fa3a156ce2985ed157adb1e50e7e25267d08cb71
yinon <yinon.bentarya@gmail.com>
2019-06-17 00:19:18 +0300

    error handling for http call

--- riscale-exercise/src/app/services/host-picker.service.ts
+++ riscale-exercise/src/app/services/host-picker.service.ts
@@ -1,21 +1,27 @@
-import { Injectable } from '@angular/core';
-import { ApiService } from './api.service';
-import { BehaviorSubject } from 'rxjs';
-import { Device } from '../models/device';
+import { Injectable } from "@angular/core";
+import { ApiService } from "./api.service";
+import { BehaviorSubject } from "rxjs";
+import { Device } from "../models/device";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HostPickerService {
   tracks$ = new BehaviorSubject<Device[]>([]);
-  
+
   constructor(private apiService: ApiService) {}
 
   getDevices() {
-    this.apiService.fetchDevices().subscribe(devices => {
-      console.log('devices: ', devices);
-      console.log('JSON devices: ', JSON.stringify(devices));
-      this.tracks$.next(devices);
-    });
+    this.apiService.fetchDevices().subscribe(
+      devices => {
+        console.log("devices: ", devices);
+        console.log("JSON devices: ", JSON.stringify(devices));
+        this.tracks$.next(devices);
+      },
+      error => {
+        alert("Server problems. See console");
+        console.log(error);
+      }
+    );
   }
 }
